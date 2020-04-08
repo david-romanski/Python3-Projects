065e1657730c4307da39915d62f189e018acb4c2
David Romanski <david_romanski@hotmail.com>
2020-04-08 17:12:41 -0400

    Update chessboard.py

--- Eloquent JavaScript/Chapter 2/chessboard.py
+++ Eloquent JavaScript/Chapter 2/chessboard.py
@@ -1,11 +1,22 @@
 # David Romanski
 # JavaScript Exercises
-# 2/24/2020
+# Created: 04/07/2020
+# Updated: 04/08/2020
 # Eloquent Javascript
 # Chapter 2: Chessboard
 # Comments: Draw a chessboard grid of size
+# Updated to allow input to determine size.
 
-size = 10;
+while True:
+	try:
+		size = int(input("Please enter a positive integer for the size of the requested chessboard: "))
+	except ValueError:
+		print("I'm sorry, I didn't understand that.")
+		continue
+	else:
+		if (size>0):
+			break
+
 for x in range(0,size):
   for y in range(0,size):
     if (x%2 == 1):
