a5e9e3464d61b8a3a1a1637662932189048a472e
David Romanski <david_romanski@hotmail.com>
2020-04-08 12:11:27 -0400

    Update FindPrimes.py

--- FindPrimes.py
+++ FindPrimes.py
@@ -3,19 +3,23 @@
 #  Find Primes
 #  Comments: This program finds all the primes of a number
 
-number1 = 1
+number1 = 144
 remainder = number1
+factorsArray = []
 
+print("Number is " + str(number1))
 
 while True:
 	tempRem = remainder
 	for x in range(2,remainder):
 		if (remainder%x == 0):
 			print (x)
+			factorsArray.append(x)
 			remainder = int(remainder/x)
 			break
 	if (tempRem == remainder):
+		factorsArray.append(remainder)
 		print(remainder)
 		break
-
+print("Prime factors are: " + str(factorsArray))
 print("done!")
