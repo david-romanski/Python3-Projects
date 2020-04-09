8b623fbbd6afe0be6eb3feac07602171fc76c90b
David Romanski <david_romanski@hotmail.com>
2020-04-09 08:48:43 -0400

    Update minimum.py

--- Eloquent JavaScript/Chapter 3/minimum.py
+++ Eloquent JavaScript/Chapter 3/minimum.py
@@ -1,18 +1,21 @@
 #  David Romanski
 #  JavaScript Exercises
-#  4/07/2020
+#  Created: 04/07/2020
+#  Updated: 04/09/2020
 #  Eloquent Javascript
 #  Chapter 3: Minimum
-#  Comments: Give 2 numbers displays the minimum number
+#  Comments: Give 2 numbers displays the minimum number.
+#  Updated to include input. Decided to not validate input because it gives neat results when you put in other things.
 
 def min(num1, num2):
   if (num1 == num2):
-    return "both are equal."
+    return "neither."
   elif (num1 > num2):
     return num2
   else: return num1
 
-num1 = 100;
-num2 = 100;
-print("Between " + str(num1) + " and " + str(num2) + " the minimum is ", end='')
+num1 = input("Please enter first value: ")
+num2 = input("Please enter second value: ")
+
+print("The minimum between " + str(num1) + " and " + str(num2) + " is ", end='')
 print(min(num1, num2))
