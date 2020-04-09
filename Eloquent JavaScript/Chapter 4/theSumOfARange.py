ffcd041c6452ba0995762f3ca47fb45f96b734b2
David Romanski <david_romanski@hotmail.com>
2020-04-09 19:38:11 -0400

    Update theSumOfARange.py

--- Eloquent JavaScript/Chapter 4/theSumOfARange.py
+++ Eloquent JavaScript/Chapter 4/theSumOfARange.py
@@ -1,10 +1,12 @@
 #  David Romanski
 #  JavaScript Exercises
-#  Updated: 04/08/2020
+#  Created: 04/08/2020
+#  Updated: 04/09/2020
 #  Eloquent Javascript
 #  Chapter 4: The Sum Of A Range
 #  Comments: Write a range function that returns an array from beginning to end inclusive. Then write a function that returns the sum of 
 #  array. As a bonus add a third argument in range that performs as a stepper.
+#  Updated to include inputs and validation.
 
 def rangeOfArray(start, end):
   rangeArray = []
@@ -29,16 +31,38 @@ def sumOfArray(rangeArray):
     sumArray += rangeArray[x]
   return sumArray
 
-print("Range from 1 to 10: ", end ='')
-print(rangeOfArray(1,10))
+while True:
+  try:
+    startRange = int(input("Please enter the start of the range: "))
+  except ValueError:
+    print("I'm sorry, I need an integer.")
+    continue
+  else:
+    break
 
-print("Range from 10 to 1: ", end ='')
-print(rangeOfArrayStep(10,1,-1))
+while True:
+  try:
+    endRange   = int(input("Please enter the end of the range: "))
+  except ValueError:
+    print("I'm sorry, I need an integer.")
+    continue
+  else:
+    break
 
-print("Range from 5 to 2 using step -1: ", end ='')
-print(rangeOfArrayStep(5, 2, -1))
+while True:
+  try:
+    stepRange  = int(input("Please enter the step: "))
+  except ValueError:
+    print("I'm sorry, I need an integer.")
+    continue
+  else:
+    if (startRange < endRange): 
+      if (stepRange > 0):
+        break
+    if (startRange > endRange): 
+      if (stepRange < 0):
+        break
+    print("I'm sorry, the step value doesn't make sense.")
 
-print("Sum from 1 to 10: " + str(sumOfArray(rangeOfArray(1,10))))
-
-print("Sum from 1 to 100 using step 5: ", end='')
-print(sumOfArray(rangeOfArrayStep(1, 100, 5)))
+print("The sum of the range is: ", end="");
+print(sumOfArray(rangeOfArrayStep(startRange,endRange,stepRange)))
