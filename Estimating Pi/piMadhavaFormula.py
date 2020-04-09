9cab142626a7a1fb1d3e718ebc24c74afe851a4f
David Romanski <david_romanski@hotmail.com>
2020-04-09 13:28:44 -0400

    Add files via upload

---
+++ Estimating Pi/piMadhavaFormula.py
@@ -0,0 +1,26 @@
+# Programmer: David Romanski
+# Pi Madhava Formula
+# Created: 04/09/2020
+# This program uses the Madhava formula to estate Pi
+
+answer = 1
+
+print(4*answer)
+
+answer -= 1/3
+
+print(4*answer)
+
+answer += 1/5
+
+print(4*answer)
+
+answer -= 1/7
+
+print(4*answer)
+
+answer += 1/9
+
+print(4*answer)
+
+answer -= 1/11
---
+++ Estimating Pi/piSecondInfinateSeries.py
@@ -0,0 +1,18 @@
+# Programmer: David Romanski
+# Pi Second Infinate Series
+# Created: 04/09/2020
+# This program uses the second infinate series to estate Pi
+
+answer  = ((2/1) * (2/3))
+
+print(2*answer)
+
+answer *= ((4/3) * (4/5))
+
+print(2*answer)
+
+answer *= ((6/5) * (6/7))
+
+print(2*answer)
+
+answer *= ((8/7) * (8/9))
