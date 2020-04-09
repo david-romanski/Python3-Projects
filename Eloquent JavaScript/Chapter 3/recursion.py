219c54dd8be8c440b1a7010d1987a7fd35b03af9
David Romanski <david_romanski@hotmail.com>
2020-04-09 10:19:47 -0400

    Update recursion.py

--- Eloquent JavaScript/Chapter 3/recursion.py
+++ Eloquent JavaScript/Chapter 3/recursion.py
@@ -1,16 +1,25 @@
 #  David Romanski
 #  JavaScript Exercises
-#  04/07/2020
+#  Created: 04/07/2020
+#  Updated: 04/09/2020
 #  Eloquent Javascript
 #  Chapter 3: Recursion
 #  Comments: Using recursion determine if a number is odd or even
+#  Updated to include input.
 
 def isEven(num):
-  if (num == 0):
+  if (abs(num) == 0):
       return "even"
-  if (num == 1):
+  if (abs(num) == 1):
       return "odd"
-  return isEven(num-2)
+  return isEven(abs(num)-2)
 
-num = 75
+while (True):
+	try:
+		num = int(input("Please enter an integer to determing if it is odd or even: "))
+	except ValueError:
+		print("I'm sorry, I need an integer.")
+		continue
+	else: break
+
 print("The number " + str(num) + " is " + isEven(num))
