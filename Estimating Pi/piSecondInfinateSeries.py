6c1d7e66f3cbfa36dc5944a13f9cf6d7adef3e2f
David Romanski <david_romanski@hotmail.com>
2020-04-12 08:49:14 -0400

    Update piSecondInfinateSeries.py

--- Estimating Pi/piSecondInfinateSeries.py
+++ Estimating Pi/piSecondInfinateSeries.py
@@ -1,18 +1,17 @@
 # Programmer: David Romanski
 # Pi Second Infinate Series
 # Created: 04/09/2020
+# Updated: 04/12/2020
 # This program uses the second infinate series to estate Pi
+# Added looping to program
 
-answer  = ((2/1) * (2/3))
-
-print(2*answer)
-
-answer *= ((4/3) * (4/5))
-
-print(2*answer)
-
-answer *= ((6/5) * (6/7))
-
-print(2*answer)
-
-answer *= ((8/7) * (8/9))
+answer = 1
+numer = 0
+denom = 1
+x = 50000
+while (x>0):
+	numer += 2
+	answer *= ((numer/denom) * (numer/(denom+2)))
+	print(2*answer)
+	denom += 2
+	x -= 1
