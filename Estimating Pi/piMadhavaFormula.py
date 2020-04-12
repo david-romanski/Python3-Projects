360719a6d58c123a2bbf9411f72b6ab5203aef53
David Romanski <david_romanski@hotmail.com>
2020-04-12 08:48:26 -0400

    Update piMadhavaFormula.py

--- Estimating Pi/piMadhavaFormula.py
+++ Estimating Pi/piMadhavaFormula.py
@@ -1,26 +1,19 @@
 # Programmer: David Romanski
 # Pi Madhava Formula
 # Created: 04/09/2020
+# Updated: 04/12/2020
 # This program uses the Madhava formula to estate Pi
+# Updated to include a loop
 
 answer = 1
 
-print(4*answer)
-
-answer -= 1/3
-
-print(4*answer)
-
-answer += 1/5
-
-print(4*answer)
-
-answer -= 1/7
-
-print(4*answer)
-
-answer += 1/9
-
-print(4*answer)
-
-answer -= 1/11
+x = 50000
+denom = 1
+while (x>0):
+	denom += 2
+	print(4*answer)
+	answer -=1/denom
+	denom += 2
+	print(4*answer)
+	answer +=1/denom
+	x -= 1
