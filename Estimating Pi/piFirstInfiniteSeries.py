768fb00e045ea35e5d51cd2962691e4e1c67f6d7
David Romanski <david_romanski@hotmail.com>
2020-04-12 09:42:41 -0400

    Update piFirstInfiniteSeries.py

--- Estimating Pi/piFirstInfiniteSeries.py
+++ Estimating Pi/piFirstInfiniteSeries.py
@@ -1,41 +1,18 @@
 # Programmer: David Romanski
 # Pi First Infinate Series
 # Created: 04/09/2020
+# Updated: 04/12/2020
 # This program uses the first infinate series to estate Pi
+# Updated this to a loop. [I did it!!!]
 
 import math
 
-one   = 2 + math.sqrt(2)
-two   = 2 + math.sqrt(one)
-three = 2 + math.sqrt(two)
-four  = 2 + math.sqrt(three)
-five  = 2 + math.sqrt(four)
-six   = 2 + math.sqrt(five)
+answer = 1
+sqrtTwo = math.sqrt(2)
+x = 40
 
-answer2 = math.sqrt(2)/2
-
-print(2/answer2)
-
-answer2 *= math.sqrt(one) / 2
-
-print(2/answer2)
-
-answer2 *= math.sqrt(two) / 2
-
-print(2/answer2)
-
-answer2 *= (math.sqrt(three)) / 2
-
-print(2/answer2)
-
-answer2 *= (math.sqrt(four)) / 2
-
-print(2/answer2)
-
-answer2 *= (math.sqrt(five)) / 2
-
-print(2/answer2)
-
-answer2 *= (math.sqrt(six)) / 2
-
-print(2/answer2)
+while (x>0):
+	answer *= sqrtTwo/2
+	print(2/answer)
+	sqrtTwo = math.sqrt(2 + sqrtTwo)
+	x -= 1
