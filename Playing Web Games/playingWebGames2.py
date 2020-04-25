e55ea9a216ee6dc1d9de3a884decd300ded6e153
David Romanski <david_romanski@hotmail.com>
2020-04-24 21:58:47 -0400

    Update playingWebGames2.py

--- Playing Web Games/playingWebGames2.py
+++ Playing Web Games/playingWebGames2.py
@@ -25,33 +25,25 @@ def loadWalkthrough():
 		# The walkthrough has spaces before commands, but no spaces before comments.
 		# So I'm removing the comments
 		if (line[0] == ' '):
-			print(line)
+			# print(line)
 			input = line.strip();
 			walkthrough.append(input)
 	print(len(walkthrough))
 	file.close()
 	return walkthrough
 
-# def createCommands():
-#	allChars = [
-#		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-#		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-#		'.', ',', ' ', 
-#		'0', '1', '2' , '3', '4', '5', '6', '7', '8', '9']
-#	walkthrough = []
-
-#	input = "";
-#	for char1 in range(0, len(allChars)):
-#		for char2 in range(0, len(allChars)):
-#			for char3 in range(0, len(allChars)):
-#				for char4 in range(0, len(allChars)):
-#					input = allChars[char1] + allChars[char2] + allChars[char3] + allChars[char4]
-#					walkthrough.append(input)
-#					print(input)
-#	return walkthrough
-
 walkthrough = loadWalkthrough()
 # print(len(walkthrough))
+
+# for x in range(0, len(walkthrough)) :
+#	print(str(x) + ': ' + walkthrough[x])
+
+# Counts the number of words in input string
+#for x in range(0, len(walkthrough)) :
+#	numWords = len(walkthrough[x].split())
+#	print("On line " + str(x) + " there are " + str(numWords) + " words.")
+
+listSecondWords = []
 
 for x in range(0, len(walkthrough)) :
 	descrip = driver.find_elements_by_tag_name('td')
@@ -64,11 +56,126 @@ for x in range(0, len(walkthrough)) :
 #	for e in descrip:
 #		print('0: ' + descrip[0].text)
 #		print('1: ' + descrip[1].text)
+
+	# This shows all the output that was scraped from the game.
 	output = descrip[1].text.splitlines()
-#		print(len(output))
+
+	# This is checking the -4 line to see if there is an error
+	if (output[len(output) - 1] == "You can't see any such thing.") :
+		print("There is an error!!!")
+		break
+
+#	for y in range(len(output), 0, -1) :
+#		print("-" + str(y) + ": " + output[len(output) - y])
 #		print('2: ' + descrip[2].text)
 
-	print(str(x) + '/' + str(len(walkthrough)) + ": test[" + walkthrough[x] + "]test")
+	# This finds if the second character in a string is upper case
+	# This then assumes that line is the label and below is the description
+#	for y in range(0, len(output)) :
+#		print(str(y) + ": " + str(output[y].isupper()))
+
+	y = len(output) - 1
+	while (not output[y].isupper()) :
+		if (not(output[y] == "")) :
+#			print("I FOUND A BLANK AT: " + str(y))
+#		else :
+			rawLine = output[y].split()
+			badChars = ['\"', '(', ')', '[', ']', '?', '.', ',', ':']
+			for i in range(0, len(rawLine)) :
+				for j in badChars :
+					rawLine[i] = rawLine[i].replace(j, '')
+			# print(rawLine)
+			listSecondWords += rawLine
+			listSecondWords = list(dict.fromkeys(listSecondWords))
+#			print(str(len(listSecondWords)) + ": ", end="")
+#			print(listSecondWords)
+			f = open("secondWordList.txt", "w")
+			for z in range(0, len(listSecondWords)) :
+				f.write(listSecondWords[z] + '\n')
+			f.close()
+		y -= 1
+	
+	# Waiting for Guggenheim's letter
+	if (walkthrough[x] == "repeatedly wait until Guggenheim gives you the letter") :
+		print(str(x) + '/' + str(len(walkthrough)) + ": test[" + walkthrough[x] + "](Waiting...)")
+		while (output[len(output) - 1].find("he pushes a letter of some kind into your hand") != -1) :
+			print(str(x) + '/' + str(len(walkthrough)) + ": test[wait](Waiting...)")
+			input_box = driver.find_element_by_name('a')
+			input_box.send_keys("wait")
+			input_box.submit()		
+
+			descrip = driver.find_elements_by_tag_name('td')
+			output = descrip[1].text.splitlines()
+			print(output[len(output) - 1])
+		x += 1
+
+	# The a4 and c2 and c1 pieces may have to be turned to work... 
+	# c1
+	if (walkthrough[x] == "put the edge piece at c1") :
+		while (output[len(output) - 1] != "[Your score has just gone up by one point.]") :
+			print(str(x) + '/' + str(len(walkthrough)) + ": test[" + walkthrough[x] + "](c1)")
+			input_box = driver.find_element_by_name('a')
+			input_box.send_keys(walkthrough[x])
+			input_box.submit()		
+
+			descrip = driver.find_elements_by_tag_name('td')
+			output = descrip[1].text.splitlines()
+			print(output[len(output) - 1])
+			if (output[len(output) - 1] == "It would project into the left-hand edge there.") :
+				input_box = driver.find_element_by_name('a')
+				input_box.send_keys("turn edge piece")
+				input_box.submit()
+			else :
+				x += 1
+
+	# a4
+	if (walkthrough[x] == "put corner piece at a4") :
+		while (output[len(output) - 1] != "[Your score has just gone up by one point.]") :
+			print(str(x) + '/' + str(len(walkthrough)) + ": test[" + walkthrough[x] + "]test")
+			input_box = driver.find_element_by_name('a')
+			input_box.send_keys(walkthrough[x])
+			input_box.submit()		
+
+			descrip = driver.find_elements_by_tag_name('td')
+			output = descrip[1].text.splitlines()
+			print(output[len(output) - 1])
+#			if (output[len(output) - 1] == "[Your score has just gone up by one point.]") :
+			if (output[len(output) - 1] == "It fits in nicely at a4.") :
+				input_box = driver.find_element_by_name('a')
+				input_box.send_keys("take a4. turn corner piece")
+				input_box.submit()
+			else :
+				x += 1
+				# Gotta clear out the output of getting a point...
+				input_box = driver.find_element_by_name('a')
+				input_box.send_keys("look")
+				input_box.submit()		
+
+				descrip = driver.find_elements_by_tag_name('td')
+				output = descrip[1].text.splitlines()
+				print("Resetti!: " + output[len(output) - 1])
+				break
+
+	# c2
+	if (walkthrough[x] == "put centre piece at c2") :
+		while (output[len(output) - 1] != "[Your score has just gone up by one point.]") :
+			print(str(x) + '/' + str(len(walkthrough)) + ": test[" + walkthrough[x] + "]test")
+			input_box = driver.find_element_by_name('a')
+			input_box.send_keys(walkthrough[x])
+			input_box.submit()		
+
+			descrip = driver.find_elements_by_tag_name('td')
+			output = descrip[1].text.splitlines()
+			print(output[len(output) - 1])
+			if (output[len(output) - 1] == "It fits in nicely at c2.") :
+				input_box = driver.find_element_by_name('a')
+				input_box.send_keys("take c2. turn centre piece")
+				input_box.submit()
+			else :
+				x += 1
+
+	# Sends walkthrough command to game
+	print(str(x) + '/' + str(len(walkthrough)) + ": test[" + walkthrough[x] + "]test(Main)")
 	input_box = driver.find_element_by_name('a')
 	input_box.send_keys(walkthrough[x])
 	input_box.submit()
