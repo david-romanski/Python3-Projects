49ea5536060b06182718e2d815545df7330335f5
David Romanski <david_romanski@hotmail.com>
2020-04-26 16:34:24 -0400

    Add files via upload

---
+++ Playing Web Games/pirateCoveAutomaton1.py
@@ -0,0 +1,210 @@
+# David Romanski
+# Started 04/26/2020
+# pirateCoveAutomaton1
+# Comments: Also known as Pirate Adventure. Just an autmated walthrough of game.
+
+import time
+import selenium
+from selenium import webdriver
+
+# Using Chrome to access web
+driver = webdriver.Chrome();
+
+# Open the website
+driver.get('https://www.ifiction.org/games/playz.php?cat=44&game=39&mode=html')
+
+def loadWalkthrough():
+	walkthrough = [	
+	"S",
+"Get Crackers",
+"Get Sneakers",
+"Get Rum",
+"Go Stairs",
+"Get Book",
+"Go Passage",
+"E",
+"Get Torch",
+"Get Duffel",
+"Open Duffel",
+"Drop Duffel",
+"Get Matches",
+"W",
+"W",
+"Read Book",
+"Go Window",
+"Say Yoho",
+"Drop Sneakers",
+"Drop Book",
+"E",
+"Go Shack",
+"Give Rum",
+"W",
+"E",
+"Go Path",
+"Go Crack",
+"Light Torch",
+"Go Shed",
+"Get Hammer",
+"Get Wings",
+"N",
+"Go Crack",
+"Unlight Torch",
+"D",
+"W",
+"W",
+"Drop Wings",
+"Drop Matches",
+"Drop Sack",
+"Drop Torch",
+"Get Book",
+"Get Sneakers",
+"Say Yoho",
+"Go Window",
+"Go Passage",
+"E",
+"pause",
+# Making sure bottle is there.
+"Get Bottle",
+"W",
+"W",
+"D",
+"Get Nails",
+"Get Rug",
+"Drop Rug",
+"Get Keys",
+"Go Stairs",
+"Go Window",
+"Say Yoho",
+"Drop Book",
+"Drop Hammer",
+"Drop Sneakers",
+"Drop Nails",
+"Get Wings",
+"Go Lagoon",
+"N",
+"Get Water",
+"Get Fish",
+"S",
+"S",
+"Drop Wings",
+"Get Torch",
+"Get Matches",
+"E",
+"E",
+"Light Torch",
+"Go Cave",
+"D",
+"Throw Fish",
+"Drop Bottle",
+"Unlock Door",
+"Go Hall",
+"E",
+"Go Shed",
+"Get Shovel",
+"N",
+"pause",
+# Checking on lumber and sails.
+"Get Lumber",
+"Get Sails",
+"W",
+"Go Pit",
+"U",
+"W",
+"Unlight Torch",
+"W",
+"W",
+"Drop Lumber",
+"Drop Sails",
+"Drop Torch",
+"Drop Matches",
+"Get Sack",
+"E",
+"Go Shack",
+"Unlock Chest",
+"Look Chest",
+"Get Plans",
+"Look Chest",
+"Get Map",
+"Get Parrot",
+"W",
+"W",
+"Drop Keys",
+"Watch Tide",
+"pause",
+# Make sure the tide is out.
+"Go Lagoon",
+"Dig Anchor",
+"Get Anchor",
+"S",
+"Drop Anchor",
+"Build Ship",
+"Drop Plans",
+"Get Sneakers",
+"Get Book",
+"Say Yoho",
+"Go Window",
+"Go Passage",
+"E",
+"Wake Pirate",
+"W",
+"W",
+"Go Window",
+"Say Yoho",
+"Drop Book",
+"Drop Sneakers",
+"Go Ship",
+"Set Sail",
+"pause",
+# You may need to wait for the tide to come in
+"Go Beach",
+"Dig",
+"Wait",
+"S",
+"E",
+"Pace 30",
+"Dig",
+"Get Box",
+"Drop Shovel",
+"Go Monstery",
+"Drop Parrot",
+"pause",
+# You may need to wait for snake to be driven off.
+
+#*** Treasure # 1 – Dubleons ***
+"Get Dubleons",
+"W",
+"W",
+"Wake Pirate",
+"N",
+"Go Ship",
+"Set Sail",
+"Go Beach",
+"Get Hammer",
+"Open Box",
+
+#*** Treasure # 2 – Stamps ***
+"Get Stamps",
+"Drop Hammer",
+"Drop Box",
+"Get Book",
+"Get Sneakers",
+"Say Yoho",
+"Go Window",
+"D",
+"Drop Dubleons",
+"Drop Stamps",
+"Score"
+	]
+	return walkthrough
+
+walkthrough = loadWalkthrough()
+print(len(walkthrough))
+
+time.sleep(3)
+for x in range(0, len(walkthrough)) :
+	if (walkthrough[x] == 'pause') :
+		pause = input("Check status.")
+	else :
+		input_box = driver.find_element_by_name('command')
+		input_box.send_keys(walkthrough[x] + '\n')
+		print(walkthrough[x])
