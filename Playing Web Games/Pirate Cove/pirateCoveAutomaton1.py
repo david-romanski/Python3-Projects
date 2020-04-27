ff48aa6ebc397f315c15a948fb7dfa9617feae6a
David Romanski <david_romanski@hotmail.com>
2020-04-27 10:06:48 -0400

    Update pirateCoveAutomaton1.py

--- Playing Web Games/Pirate Cove/pirateCoveAutomaton1.py
+++ Playing Web Games/Pirate Cove/pirateCoveAutomaton1.py
@@ -1,7 +1,7 @@
 # David Romanski
 # Started 04/26/2020
 # pirateCoveAutomaton1
-# Comments: Also known as Pirate Adventure. Just an autmated walthrough of game.
+# Comments: Also known as Pirate Adventure. Just an automated walkthrough of game.
 
 import time
 import selenium
@@ -14,323 +14,179 @@ driver = webdriver.Chrome();
 driver.get('https://www.ifiction.org/games/playz.php?cat=44&game=39&mode=html')
 
 def loadWalkthrough():
-	walkthrough = [	
-	"S",
-	"Get Crackers",
-	"Get Sneakers",
-	"Get Rum",
-	"Go Stairs",
-	"Get Book",
-	"Go Passage",
-	"E",
-	"Get Torch",
-	"Get Duffel",
-	"Open Duffel",
-	"Drop Duffel",
-	"Get Matches",
-	"W",
-	"W",
-	"Read Book",
-	"Go Window",
-	"Say Yoho",
-	"Drop Sneakers",
-	"Drop Book",
-	"E",
-	"Go Shack",
-	"Give Rum",
-	"W",
-	"E",
-	"Go Path",
-	"Go Crack",
-	"Light Torch",
-	"Go Shed",
-	"Get Hammer",
-	"Get Wings",
-	"N",
-	"Go Crack",
-	"Unlight Torch",
-	"D",
-	"W",
-	"W",
-	"Drop Wings",
-	"Drop Matches",
-	"Drop Sack",
-	"Drop Torch",
-	"Get Book",
-	"Get Sneakers",
-	"Say Yoho",
-	"Go Window",
-	"Go Passage",
-	"E",
-	"Get Bottle",
-	"W",
-	"W",
-	"D",
-	"Get Nails",
-	"Get Rug",
-	"Drop Rug",
-	"Get Keys",
-	"Go Stairs",
-	"Go Window",
-	"Say Yoho",
-	"Drop Book",
-	"Drop Hammer",
-	"Drop Sneakers",
-	"Drop Nails",
-	"Get Wings",
-	"Go Lagoon",
-	"N",
-	"get Water",
-	"Get Fish",
-	"S",
-	"S",
-	"Drop Wings",
-	"Get Torch",
-	"Get Matches",
-	"E",
-	"E",
-	"Light Torch",
-	"Go Cave",
-	"D",
-	"Throw Fish",
-	"Drop Bottle",
-	"Unlock Door",
-	"Go Hall",
-	"E",
-	"Go Shed",
-	"Get Shovel",
-	"N",
-	"Get Lumber",
-	"Get Sails",
-	"W",
-	"Go Pit",
-	"U",
-	"W",
-	"Unlight Torch",
-	"W",
-	"W",
-	"Drop Lumber",
-	"Drop Sails",
-	"Drop Torch",
-	"Drop Matches",
-	"Get Sack",
-	"E",
-	"Go Shack",
-	"Unlock Chest",
-	"Look Chest",
-	"Get Plans",
-	"Look Chest",
-	"Get Map",
-	"Get Parrot",
-	"W",
-	"W",
-	"Drop Keys",
-	"Watch Tide",
-	"Go Lagoon",
-	"Dig Anchor",
-	"Get Anchor",
-	"S",
-	"Drop Anchor",
-	"Build Ship",
-	"Drop Plans",
-	"Get Sneakers",
-	"Get Book",
-	"Say Yoho",
-	"Go Window",
-	"Go Passage",
-	"E",
-	"Wake Pirate",
-	"W",
-	"W",
-	"Go Window",
-	"Say Yoho",
-	"Drop Book",
-	"Drop Sneakers",
-	"Go Ship",
-	"Set Sail",
-	"Go Beach",
-	"DIG",
-	"S",
-	"E",
-	"Pace 30",
-	"Dig",
-	"Get Box",
-	"Drop Shovel",
-	"Go Monstery",
-	"Drop Parrot",
-
-	#*** Treasure # 1 – Dubleons ***
-	"Get Dubleons",
-	"W",
-	"W",
-	"Wake Pirate",
-	"N",
-	"Go Ship",
-	"Set Sail",
-	"Go Beach",
-	"Get Hammer",
-	"Open Box",
-
-	#*** Treasure # 2 – Stamps ***
-	"Get Stamps",
-	"Drop Hammer",
-	"Drop Box",
-	"Get Book",
-	"Get Sneakers",
-	"Say Yoho",
-	"Go Window",
-	"D",
-	"Drop Dubleons",
-	"Drop Stamps",
-	"Score"
-	]
+###################################################################################################
+# This def loads a walkthrough file.
+# The walkthrough has spaces before commands, but no spaces before comments.
+# So I'm removing the comments
+###################################################################################################
+	walkthrough = []
+	file = open("pirateCoveWT.txt", "r")
+	for line in file:
+		if (line[0] == ' '):
+			input = line.strip();
+			walkthrough.append(input)
+	file.close()
 	return walkthrough
+####################################### end loadWalkthrough #######################################
+
+def waitOnly(command, phrase, actualCommands) :
+	input_box = driver.find_element_by_name('command')
+	input_box.send_keys(command + '\n')
+	actualCommands += 1
+	descrip = driver.find_elements_by_tag_name('td')
+	output = descrip[1].text.splitlines()
+	print("-1: " + output[len(output) - 1])
+	print("-2: " + output[len(output) - 2])
+	print("-3: " + output[len(output) - 3])
+	print("-4: " + output[len(output) - 4])
+	while  ((output[len(output) - 2].find(phrase) == -1) 
+		and (output[len(output) - 3].find(phrase) == -1)
+		and (output[len(output) - 4].find(phrase) == -1)) :
+		print('wait')
+		input_box = driver.find_element_by_name('command')
+		input_box.send_keys('wait\n')
+		actualCommands += 1
+		descrip = driver.find_elements_by_tag_name('td')
+		output = descrip[1].text.splitlines()
+		print("-1: " + output[len(output) - 1])
+		print("-2: " + output[len(output) - 2])
+		print("-3: " + output[len(output) - 3])
+		print("-4: " + output[len(output) - 4])
+
+def waitAndCommand(command, phrase, actualCommands) :
+	input_box = driver.find_element_by_name('command')
+	input_box.send_keys(command + '\n')
+	actualCommands += 1
+	descrip = driver.find_elements_by_tag_name('td')
+	output = descrip[1].text.splitlines()
+	print("-1: " + output[len(output) - 1])
+	print("-2: " + output[len(output) - 2])
+	print("-3: " + output[len(output) - 3])
+	print("-4: " + output[len(output) - 4])
+	while  ((output[len(output) - 2].find(phrase) == -1) 
+		and (output[len(output) - 3].find(phrase) == -1)
+		and (output[len(output) - 4].find(phrase) == -1)) :
+		print('wait')
+		input_box = driver.find_element_by_name('command')
+		input_box.send_keys('wait\n')
+		actualCommands += 1
+		print(command)
+		input_box = driver.find_element_by_name('command')
+		input_box.send_keys(command + '\n')
+		actualCommands += 1
+		descrip = driver.find_elements_by_tag_name('td')
+		output = descrip[1].text.splitlines()
+		print("-1: " + output[len(output) - 1])
+		print("-2: " + output[len(output) - 2])
+		print("-3: " + output[len(output) - 3])
+		print("-4: " + output[len(output) - 4])
 
 walkthrough = loadWalkthrough()
 totalCommands = len(walkthrough)
 print(totalCommands)
 
-time.sleep(3)
+actualCommands = 0
+
 for x in range(0, len(walkthrough)) :
-	if (walkthrough[x] == 'pause') :
-		pause = input("Check status.")
+	if (walkthrough[x] == "Give Rum") :
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+		waitOnly(walkthrough[x],"Pirate grabs rum", actualCommands)
+
+	elif (walkthrough[x] == "Throw Fish") :
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+		waitOnly(walkthrough[x], "Crocs eat fish", actualCommands)
+
+	elif (walkthrough[x] == "Watch Tide") :
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+		input_box = driver.find_element_by_name('command')
+		input_box.send_keys(walkthrough[x] + '\n')
+		actualCommands += 1
+		descrip = driver.find_elements_by_tag_name('td')
+		output = descrip[1].text.splitlines()
+		print("-1: " + output[len(output) - 1])
+		print("-2: " + output[len(output) - 2])
+		print("-3: " + output[len(output) - 3])
+		print("-4: " + output[len(output) - 4])
+		while  (((output[len(output) - 2].find("tide is out") == -1) 
+			and  (output[len(output) - 3].find("tide is out") == -1)
+			and  (output[len(output) - 4].find("tide is out") == -1))
+			or ((output[len(output) -2].find("Tides be a changing matey") != -1)
+			or  (output[len(output) -3].find("Tides be a changing matey") != -1))) :
+			print('wait')
+			input_box = driver.find_element_by_name('command')
+			input_box.send_keys('wait\n')
+			actualCommands += 1
+			print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+			input_box = driver.find_element_by_name('command')
+			input_box.send_keys(walkthrough[x] + '\n')
+			actualCommands += 1
+			descrip = driver.find_elements_by_tag_name('td')
+			output = descrip[1].text.splitlines()
+			print("-1: " + output[len(output) - 1])
+			print("-2: " + output[len(output) - 2])
+			print("-3: " + output[len(output) - 3])
+			print("-4: " + output[len(output) - 4])
+
+	elif (walkthrough[x] == "Go Lagoon") :
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+		input_box = driver.find_element_by_name('command')
+		input_box.send_keys(walkthrough[x] + '\n')
+		actualCommands += 1
+		descrip = driver.find_elements_by_tag_name('td')
+		output = descrip[1].text.splitlines()
+		print("-1: " + output[len(output) - 1])
+		print("-2: " + output[len(output) - 2])
+		print("-3: " + output[len(output) - 3])
+		print("-4: " + output[len(output) - 4])
+		if    ((output[len(output) - 2].find("Tides be a changing matey") != -1)
+			or (output[len(output) - 3].find("Tides be a changing matey") != -1)
+			or (output[len(output) - 4].find("Tides be a changing matey") != -1)) :
+			input_box = driver.find_element_by_name('command')
+			input_box.send_keys('S\n')
+			actualCommands += 1
+			print('S')
+			# Reset x back to 'Watch Tide'
+			x -= 1
+			print("watch tide test: " + walkthrough[x])
+
+	elif (walkthrough[x] == "Dig Anchor") :
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+		input_box = driver.find_element_by_name('command')
+		input_box.send_keys(walkthrough[x] + '\n')
+		actualCommands += 1
+		descrip = driver.find_elements_by_tag_name('td')
+		output = descrip[1].text.splitlines()
+		print("-1: " + output[len(output) - 1])
+		print("-2: " + output[len(output) - 2])
+		print("-3: " + output[len(output) - 3])
+		print("-4: " + output[len(output) - 4])
+		while  ((output[len(output) - 2].find("Tides be a changing matey") != -1) 
+			or (output[len(output) - 3].find("Tides be a changing matey") != -1)
+			or (output[len(output) - 4].find("Tides be a changing matey") != -1)) :
+			input_box = driver.find_element_by_name('command')
+			input_box.send_keys('S\n')
+			actualCommands += 1
+			print('S')
+			# Reset x back to 'Watch Tide'
+			x -= 2
+			print("dig anchor test: " + walkthrough[x])
+
+	elif (walkthrough[x] == "Set Sail") :
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+		waitAndCommand(walkthrough[x], "After a day at sea", actualCommands)
+
+	elif (walkthrough[x] == "DIG") :
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+		waitOnly(walkthrough[x], "Pirate grabs rum", actualCommands)
+
+	elif (walkthrough[x] == "Drop Parrot") :
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
+		waitOnly(walkthrough[x], "Parrot attacks snakes", actualCommands)
+
 	else :
+		input_box = driver.find_element_by_name('command')
+		input_box.send_keys(walkthrough[x] + '\n')
+		actualCommands += 1
+		print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
 
-		if (walkthrough[x] == "Give Rum") :
-			input_box = driver.find_element_by_name('command')
-			input_box.send_keys(walkthrough[x] + '\n')
-			print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-			descrip = driver.find_elements_by_tag_name('td')
-			output = descrip[1].text.splitlines()
-			print("-1: " + output[len(output) - 1])
-			print("-2: " + output[len(output) - 2])
-			print("-3: " + output[len(output) - 3])
-			while ((output[len(output) - 2].find("Pirate grabs rum") == -1) and (output[len(output) - 3].find("Pirate grabs rum") == -1)):
-				input_box = driver.find_element_by_name('command')
-				input_box.send_keys('wait\n')
-				print('wait')
-				descrip = driver.find_elements_by_tag_name('td')
-				output = descrip[1].text.splitlines()
-				print("-1: " + output[len(output) - 1])
-				print("-2: " + output[len(output) - 2])
-				print("-3: " + output[len(output) - 3])				
-
-
-		elif (walkthrough[x] == "Throw Fish") :
-			input_box = driver.find_element_by_name('command')
-			input_box.send_keys(walkthrough[x] + '\n')
-			print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-			descrip = driver.find_elements_by_tag_name('td')
-			output = descrip[1].text.splitlines()
-			print("-1: " + output[len(output) - 1])
-			print("-2: " + output[len(output) - 2])
-			print("-3: " + output[len(output) - 3])
-			print("-4: " + output[len(output) - 4])
-			while (output[len(output) - 2].find("Crocs eat fish") == -1) :
-				input_box = driver.find_element_by_name('command')
-				input_box.send_keys('wait\n')
-				print('wait')
-				descrip = driver.find_elements_by_tag_name('td')
-				output = descrip[1].text.splitlines()
-				print("-1: " + output[len(output) - 1])
-				print("-2: " + output[len(output) - 2])
-				print("-3: " + output[len(output) - 3])				
-				print("-4: " + output[len(output) - 4])				
-
-		elif (walkthrough[x] == "Watch Tide") :
-			input_box = driver.find_element_by_name('command')
-			input_box.send_keys(walkthrough[x] + '\n')
-			print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-			descrip = driver.find_elements_by_tag_name('td')
-			output = descrip[1].text.splitlines()
-			print("-1: " + output[len(output) - 1])
-			print("-2: " + output[len(output) - 2])
-			print("-3: " + output[len(output) - 3])
-			print("-4: " + output[len(output) - 4])
-			while ((output[len(output) - 2].find("tide is out") == -1) 
-				and (output[len(output) - 3].find("tide is out") == -1)
-				and (output[len(output) - 4].find("tide is out") == -1)) :
-				input_box = driver.find_element_by_name('command')
-				input_box.send_keys('wait\n')
-				print('wait')
-				input_box = driver.find_element_by_name('command')
-				input_box.send_keys(walkthrough[x] + '\n')
-				print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-				descrip = driver.find_elements_by_tag_name('td')
-				output = descrip[1].text.splitlines()
-				print("-1: " + output[len(output) - 1])
-				print("-2: " + output[len(output) - 2])
-				print("-3: " + output[len(output) - 3])
-				print("-4: " + output[len(output) - 4])
-
-		elif (walkthrough[x] == "Set Sail") :
-			input_box = driver.find_element_by_name('command')
-			input_box.send_keys(walkthrough[x] + '\n')
-			print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-			descrip = driver.find_elements_by_tag_name('td')
-			output = descrip[1].text.splitlines()
-			print("-1: " + output[len(output) - 1])
-			print("-2: " + output[len(output) - 2])
-			print("-3: " + output[len(output) - 3])
-			print("-4: " + output[len(output) - 4])
-			while ((output[len(output) -3].find("After a day at sea") == -1) and (output[len(output) -4].find("After a day at sea") == -1)) :
-				input_box = driver.find_element_by_name('command')
-				input_box.send_keys('wait\n')
-				print('wait')
-				input_box = driver.find_element_by_name('command')
-				input_box.send_keys(walkthrough[x] + '\n')
-				print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-				descrip = driver.find_elements_by_tag_name('td')
-				output = descrip[1].text.splitlines()
-				print("-1: " + output[len(output) - 1])
-				print("-2: " + output[len(output) - 2])
-				print("-3: " + output[len(output) - 3])
-				print("-4: " + output[len(output) - 4])
-
-		elif (walkthrough[x] == "DIG") :
-			input_box = driver.find_element_by_name('command')
-			input_box.send_keys(walkthrough[x] + '\n')
-			print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-			descrip = driver.find_elements_by_tag_name('td')
-			output = descrip[1].text.splitlines()
-			print("-1: " + output[len(output) - 1])
-			print("-2: " + output[len(output) - 2])
-			print("-3: " + output[len(output) - 3])
-			print("-4: " + output[len(output) - 4])
-			while ((output[len(output) -2].find("Pirate grabs rum") == -1) and (output[len(output) -3].find("Pirate grabs rum") == -1)) :
-				input_box = driver.find_element_by_name('command')
-				input_box.send_keys('wait\n')
-				print('wait')
-				descrip = driver.find_elements_by_tag_name('td')
-				output = descrip[1].text.splitlines()
-				print("-1: " + output[len(output) - 1])
-				print("-2: " + output[len(output) - 2])
-				print("-3: " + output[len(output) - 3])
-				print("-4: " + output[len(output) - 4])
-
-		elif (walkthrough[x] == "Drop Parrot") :
-			input_box = driver.find_element_by_name('command')
-			input_box.send_keys(walkthrough[x] + '\n')
-			print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-			descrip = driver.find_elements_by_tag_name('td')
-			output = descrip[1].text.splitlines()
-			print("-1: " + output[len(output) - 1])
-			print("-2: " + output[len(output) - 2])
-			print("-3: " + output[len(output) - 3])
-			print("-4: " + output[len(output) - 4])
-			while ((output[len(output) -2].find("Parrot attacks snakes") == -1) and (output[len(output) -3].find("Parrot attacks snakes") == -1)):
-				input_box = driver.find_element_by_name('command')
-				input_box.send_keys('wait\n')
-				print('wait')
-				descrip = driver.find_elements_by_tag_name('td')
-				output = descrip[1].text.splitlines()
-				print("-1: " + output[len(output) - 1])
-				print("-2: " + output[len(output) - 2])
-				print("-3: " + output[len(output) - 3])
-				print("-4: " + output[len(output) - 4])
-
-		else :
-			input_box = driver.find_element_by_name('command')
-			input_box.send_keys(walkthrough[x] + '\n')
-			print(str(x) + '/' + str(totalCommands) + ": " + walkthrough[x])
-			time.sleep(1)
+print("Actual Commands used: " + str(actualCommands))
